77b8c1b2c33c9c60086477ace44d50b111ab3c73
NguyenTruongNhu <nhu81632@gmail.com>
2024-03-25 21:55:00 +0700

    success full payment order

--- src/component/state/Order/Action.js
+++ src/component/state/Order/Action.js
@@ -16,9 +16,9 @@ export const createOrder = (reqData) => async (dispatch) => {
         Authorization: `Bearer ${reqData.jwt}`,
       },
     });
-    // if (data.payment_url) {
-    //   window.location.href = data.payment_url;
-    // }
+    if (data.payment_url) {
+      window.location.href = data.payment_url;
+    }
     console.log("create order data", data);
     dispatch({ type: CREATE_ORDER_SUCCESS, payload: data });
   } catch (error) {
